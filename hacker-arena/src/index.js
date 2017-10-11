e0296d8f5d9f39b2f9799d0f21f2ab2f544e1148
Paul Brown <paabrown@gmail.com>
2017-10-11 16:44:53 -0400

    Hook up to firebase, set up problems in database

--- hacker-arena/src/index.js
+++ hacker-arena/src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './Styles/index.css';
 import App from './App';
 
+console.log('index.js is running')
+
 ReactDOM.render(<App />, document.getElementById('root'));
