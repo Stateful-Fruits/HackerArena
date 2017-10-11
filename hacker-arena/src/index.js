c23a3a2cbc7a166035e84c59a5f397de529d5fb6
Paul Brown <paabrown@gmail.com>
2017-10-11 17:37:03 -0400

    Complete database with users and rooms

--- hacker-arena/src/index.js
+++ hacker-arena/src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import './Styles/index.css';
 import App from './App';
 
-console.log('index.js is running')
-
 ReactDOM.render(<App />, document.getElementById('root'));
