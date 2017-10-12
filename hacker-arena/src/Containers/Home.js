b249e4a1428c9d1ef66ae424f8ccec6ac1614f9b
ccolin84 <ccolin84@gmail.com>
2017-10-12 16:51:45 -0400

    updated home.js file paths

--- hacker-arena/src/Containers/Home.js
+++ hacker-arena/src/Containers/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 
-import fire from '../firebase';
+import fire from '../Firebase/firebase';
 
 import GameRoomList from '../Components/GameRooms/GameRoomList';
 
