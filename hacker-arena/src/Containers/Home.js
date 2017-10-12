dfda2dfa4d6d6bdf697267eaace7915510f2426a
ccolin84 <ccolin84@gmail.com>
2017-10-12 14:22:14 -0400

    update redux flow

--- hacker-arena/src/Containers/Home.js
+++ hacker-arena/src/Containers/Home.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 
 import GameRoomList from '../Components/GameRooms/GameRoomList';
+
+import updateGameRooms from '../Actions/updateGameRooms';
 
 const sampleGameRooms = [
   {roomName: 'Paul\'s Game'},
@@ -11,18 +13,38 @@ const sampleGameRooms = [
   {roomName: 'David\'s Game'},
 ];
 
-const Home = ({ gameRooms, dispatch }) => (
-  <div>
-    <button onClick={ () => dispatch(push('/About')) }><h1>About</h1></button>
-    <h2>Welcome to Hacker Arena</h2>
-    <GameRoomList 
-      gameRooms={gameRooms}
-    />
-  </div>
-);
+class Home extends Component {
+  componentWillMount() {
+    console.log(this.props);
+    let { updateGameRooms } = this.props;
+    updateGameRooms();
+  }
 
-const mapStateToProps = (state) => ({
-  gameRooms: state.gameRooms
+  render() {
+    let { gameRooms, navigateToAbout } = this.props;
+    return gameRooms ? (
+       <div>
+         <button onClick={ navigateToAbout }><h1>About</h1></button>
+         <h2>Welcome to Hacker Arena</h2>
+         <GameRoomList 
+           gameRooms={gameRooms}
+           navigateToAbout={navigateToAbout}
+         />
+       </div>
+      ) : null;
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log('map state to props state passed: ', state);
+  return ({
+  gameRooms: state.gameRooms.gameRooms
+});
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  updateGameRooms: () => dispatch(updateGameRooms(sampleGameRooms)),
+  navigateToAbout: () => dispatch(push('/About'))
 });
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
