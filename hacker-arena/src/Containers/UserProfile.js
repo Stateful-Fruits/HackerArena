5d31afca971752ec896ea598098a249cdbaf7329
kinyusui <kaixngchen@gmail.com>
2017-10-13 16:05:01 -0400

    simple userprofiles

--- hacker-arena/src/Containers/UserProfile.js
+++ hacker-arena/src/Containers/UserProfile.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import updateUserProfile from '../Actions/updateUserProfile';
 
 class UserProfile extends React.Component {
   render () {
     let props = this.props;
+    let password = '', message = '';
+    if (props.showPass) {
+      password = props.password;
+      message = 'Hide Password';
+    } else {
+      password = '*'.repeat(props.password.length);
+      message = 'Show Password';
+    }
+
     return (
       <div>
-        <div>Username: {props.username}</div>
-        <div>Won: {props.won}</div>
+        <div>Username: {props.username}</div><br/>
+        <div>Password: {password}</div>
+        <button onClick={props.update}
+          value='SHOW_PASS'>{message}</button><br/>
+        <div>Won: {props.won}</div><br/>
         <div>Lost: {props.lost}</div>
       </div>
     )
@@ -17,15 +30,20 @@ class UserProfile extends React.Component {
 const mapStateToProps = (state) => {
   return {
     username: state.user.username,
+    password: state.user.password,
+    showPass: state.user.showPass,
     won: state.user.won,
     lost: state.user.lost
   }
 };
 
-// const mapDispatcherToProps = (dispatch) => {
-//   return {
-    
-//   }
-// }
+const mapDispatcherToProps = (dispatch) => {
+  return {
+    update: (e) => {
+      e.preventDefault();
+      dispatch(updateUserProfile());
+    }
+  }
+}
 
-export default connect(mapStateToProps)(UserProfile);
+export default connect(mapStateToProps, mapDispatcherToProps)(UserProfile);
