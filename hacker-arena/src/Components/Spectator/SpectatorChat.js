3015d18db1d9d91c6b816d4d3ac9bd4d0a4f5b5c
ccolin84 <ccolin84@gmail.com>
2017-10-16 13:45:36 -0400

    add chat and and update spectator list in real time

--- hacker-arena/src/Components/Spectator/SpectatorChat.js
+++ hacker-arena/src/Components/Spectator/SpectatorChat.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
 
+import fire from '../../Firebase/firebase';
+
 import SpectatorChatMessage from './SpectatorChatMessage.js';
 
 class SpectatorChat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: ''
+    }
+    this.handleMsgInput = this.handleMsgInput.bind(this);
+    this.handleMsgSend = this.handleMsgSend.bind(this);
+  }
+
+  handleMsgInput(e) {
+    this.setState({ msg: e.target.value });
+  }
+
+  handleMsgSend(e) {
+    e.preventDefault();
+    let msg = this.state.msg;
+    let room = this.props.gameRoom;
+    let username = fire.auth().currentUser.email.split('@')[0] || 'UnkownUser';
+    this.props.sendSpectatorMessage(room, username, msg);
+  }
+
   render() {
+    let { gameRoom } = this.props;
+    let spectatorChat = gameRoom.spectatorChat || [];
     return (
-      <div>
-        <h2>Chat Here!</h2>
-        <input type="text" />
+      <div style={{ margin: "5%" }}>
+        <form onSubmit={this.handleMsgSend}>
+          <h2>Chat Here!</h2>
+          <input type="text" onChange={this.handleMsgInput} />
+          <button>
+            <h3>Send</h3>
+          </button>
+        </form>
         <div>
-          { [1,2,3].map((i) => <SpectatorChatMessage key={i}/>) }
+          { spectatorChat.map((chatMessage, i) => (
+            <SpectatorChatMessage 
+              key={chatMessage.msg+i}
+              chatMessage={chatMessage}
+            /> 
+          ))}
         </div>
       </div>
     );
