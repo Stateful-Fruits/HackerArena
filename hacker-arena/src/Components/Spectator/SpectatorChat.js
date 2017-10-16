06a45f194d0bd3b36be60820fd36082ff2bc03e4
David Zou <davidzsy20@Davids-MacBook-Pro.local>
2017-10-16 19:33:28 -0400

    add vchat partial

--- hacker-arena/src/Components/Spectator/SpectatorChat.js
+++ hacker-arena/src/Components/Spectator/SpectatorChat.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-
+import '../../Styles/SpectatorChat.css'
 import fire from '../../Firebase/firebase';
-
+import SimpleWebRTC from "simplewebrtc"
 import SpectatorChatMessage from './SpectatorChatMessage.js';
 
 class SpectatorChat extends Component {
@@ -12,6 +12,7 @@ class SpectatorChat extends Component {
     }
     this.handleMsgInput = this.handleMsgInput.bind(this);
     this.handleMsgSend = this.handleMsgSend.bind(this);
+    this.chat = this.chat.bind(this);
   }
 
   handleMsgInput(e) {
@@ -25,12 +26,26 @@ class SpectatorChat extends Component {
     let username = fire.auth().currentUser.email.split('@')[0] || 'UnkownUser';
     this.props.sendSpectatorMessage(room, username, msg);
   }
-
+  chat(e) {
+    var webrtc = new SimpleWebRTC({
+      // the id/element dom element that will hold "our" video 
+      localVideoEl: 'localVideo',
+      // the id/element dom element that will hold remote videos 
+      remoteVideosEl: 'remoteVideos',
+      // immediately ask for camera access 
+      autoRequestMedia: true
+  });
+  webrtc.on('readyToCall', function () {
+    // you can name it anything 
+    webrtc.joinRoom('your awesome room name');
+});
+  }
   render() {
     let { gameRoom } = this.props;
     let spectatorChat = gameRoom.spectatorChat || [];
     return (
       <div style={{ margin: "5%" }}>
+      <button onClick= {(e)=>this.chat()}>vChat</button>
         <form onSubmit={this.handleMsgSend}>
           <h2>Chat Here!</h2>
           <input type="text" onChange={this.handleMsgInput} />
@@ -38,7 +53,10 @@ class SpectatorChat extends Component {
             <h3>Send</h3>
           </button>
         </form>
+       
         <div>
+          <div className='stream'> <video id ="localVideo"></video>
+          </div>
           { spectatorChat.map((chatMessage, i) => (
             <SpectatorChatMessage 
               key={chatMessage.msg+i}
