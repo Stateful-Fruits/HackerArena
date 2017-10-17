2e11634430cec77bb340fe85e17c80438f124908
Paul Brown <paabrown@gmail.com>
2017-10-17 12:46:45 -0400

    Allow user to upload photos

--- hacker-arena/src/Components/User/EditProfile.js
+++ hacker-arena/src/Components/User/EditProfile.js
@@ -9,11 +9,15 @@ class EditProfile extends React.Component {
     super(props);
 
     this.state = {
-      photoURL: fire.auth().currentUser.photoURL
+      photoURL: fire.auth().currentUser.photoURL,
+      photoFiles: []
     }
 
     this.onSubmit = this.onSubmit.bind(this);
     this.onChange =this.onChange.bind(this);
+
+    this.onFileChange =this.onFileChange.bind(this);    
+    this.onFileSubmit =this.onFileSubmit.bind(this);
   }
 
   onChange(e) {
@@ -27,20 +31,43 @@ class EditProfile extends React.Component {
     fire.auth().currentUser.updateProfile({
       photoURL: photoURL
     });
+  }
 
+  onFileChange(e) {
+    console.log('e.target.files[0]', e.target.files[0])
+    console.log('e.target.slice', [].slice.call(e.target.files))
+    console.log('e.target.slice[0]', [].slice.call(e.target.files)[0])
+    console.log('e.name', e.target.name)
+    this.setState({photoFiles: [].slice.call(e.target.files)});
+  }
+
+  onFileSubmit(e) {
+    e.preventDefault();
+    let file = this.state.photoFiles[0];
+    console.log('typeof file', typeof file);
+    console.log('file', file)
+    let storageRef = fire.storage().ref('user-photos/' + file.name);
+    storageRef.put(file);
   }
 
   render() {
     return (
       <div className="edit-profile">
-        Enter custom url to profile photo here:
-        <input className="photo-url-input" onChange={this.onChange}
-          value = {this.state.photoURL}
-          type= 'text'
-          name= 'photoURL'
-          className = 'form-control'
-        />
-        <button onClick={this.onSubmit}>Submit</button>
+        <div className="photo-url-edit">
+          Enter custom url to profile photo here:
+          <input className="photo-url-input" onChange={this.onChange}
+            value = {this.state.photoURL}
+            type= 'text'
+            name= 'photoURL'
+            className = 'form-control'
+          />
+          <button onClick={this.onSubmit}>Submit</button>
+        </div>
+        <div className="upload-photo">
+          <input type="file" id="input" name="photoFiles" files={this.state.photoFiles} onChange={this.onFileChange}/>
+          file:{JSON.stringify(this.state.photoFiles)}
+        </div>
+        <button onClick={this.onFileSubmit}>Upload</button>
       </div>
     )
   }
