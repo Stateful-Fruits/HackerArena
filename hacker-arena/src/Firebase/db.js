e265d68b407e40bd06820107d38cdbbf5c2784be
Paul Brown <paabrown@gmail.com>
2017-10-18 12:14:25 -0400

    Implement debug mode

--- hacker-arena/src/Firebase/db.js
+++ hacker-arena/src/Firebase/db.js
@@ -1,9 +1,11 @@
 import fire from './firebase.js';
+import populateDB from './dbFiller/populateDb.js';
 
 const db = {};
 
 db.Users = fire.database().ref('users');
 db.Problems = fire.database().ref('problems');
 db.Rooms = fire.database().ref('rooms');
+db.populateDB = populateDB;
 
 export default db;
