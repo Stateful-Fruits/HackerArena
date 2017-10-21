ced1d294b260398891d19a4caeb564c3a7355ea4
David Zou <davidzsy20@Davids-MacBook-Pro.local>
2017-10-21 11:55:15 -0400

    DriverRoom NavigatorRoom components

---
+++ hacker-arena/src/Containers/Pair/DriverRoom.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import fire from './../../Firebase/firebase';
+import CodeEditor from '../../Components/CodeEditor/CodeEditor.js'; //From Simon
+import ProgressBar from '../../Components/GameRoom/ProgressBar';
+
+//this should better be a view instead of room;
+class DriverRoom extends React.Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+            allowEnter: true
+          }
+
+        }
+    render() {
+        let {roomId} = this.props
+        return (
+          <div>
+            <ProgressBar room={roomId}/>
+            <div id="editorAndTestSuite">
+            <CodeEditor currentRoom={roomId}/>
+            </div>
+          </div>
+        )
+    }
+    }
+const mapStateToProps = (state) => ({
+            roomId: state.router.location.pathname.split('/')[2],
+            username: fire.auth().currentUser.email.split('@')[0],          
+          });
+export default connect(mapStateToProps)(DriverRoom);
+          
