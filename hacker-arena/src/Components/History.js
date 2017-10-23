6cf4dfc359a3d05b7599ab965be1baf32bf7faa1
Simon K Zheng <simonkzheng@gmail.com>
2017-10-23 11:22:17 -0400

    Calendar tooltips + History

---
+++ hacker-arena/src/Components/History.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import $ from 'jquery';
+
+const History = (props) => {  
+  let games = props.profile.history;
+
+  return (
+    <div>
+      
+    </div>
+)};
+
+export default History;
