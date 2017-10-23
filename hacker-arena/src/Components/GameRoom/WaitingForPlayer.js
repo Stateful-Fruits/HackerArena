a89e6c8e23a409075a94ae9fe3c6c4f18a497c17
Paul Brown <paabrown@gmail.com>
2017-10-23 12:30:07 -0400

    Fix rendering of waiting room so that it shows the teams and who is on which team

--- hacker-arena/src/Components/GameRoom/WaitingForPlayer.js
+++ hacker-arena/src/Components/GameRoom/WaitingForPlayer.js
@@ -2,21 +2,63 @@ import React, { Component } from 'react';
 
 class WaitingForPlayer extends Component { 
   render() {
+    console.log('this.props in waiting', this.props)
     let playerSpans = [];
     if (this.props.room) {
-      let { room } = this.props;
+      console.log('this.props.room is running', this.props.room)
+      let { room, teams } = this.props;
       let { playerCapacity } = room;
       let players = room.players || {};
       let playerNames = Object.keys(players);
-      for (let i = 0; i < playerCapacity; i++) {
-        playerSpans.push(
-          <li className='list-group-item' key={(playerNames[i] || "OPEN") + i}>
-            <h4>
-              Player {i} -> { playerNames[i] ? playerNames[i] : <span style={{color: 'darkgreen'}}>OPEN</span> }
-            </h4>
-          </li>);
+      console.log('teams', teams);
+
+      if (room.isPairRoom) {
+        for (let i = 0; i < room.maxPairs; i++) {
+          playerSpans.push(
+            <li className='list-group-item' key={'team' + i}>
+              <h4>
+                Team {i} -> {
+                  <div>
+                    <div>
+                      Driver: {
+                        teams[i] && teams[i].driver ? 
+                          teams[i].driver
+                          : 
+                          <span style={{color: 'darkgreen'}}>
+                            OPEN
+                          </span>
+                      }
+                    </div>
+                    <div>
+                      Navigator: {
+                        teams[i] && teams[i].navigator ? 
+                          teams[i].navigator 
+                          :
+                          <span style={{color: 'darkgreen'}}>
+                          OPEN
+                          </span>                  
+                      }
+                    </div>
+                  </div>
+                }
+              </h4>
+            </li>
+          );
+        }
+      } else {
+        for (let i = 0; i < playerCapacity; i++) {
+          playerSpans.push(
+            <li className='list-group-item' key={(playerNames[i] || "OPEN") + i}>
+              <h4>
+                Player {i} -> { playerNames[i] ? playerNames[i] : <span style={{color: 'darkgreen'}}>OPEN</span> }
+              </h4>
+            </li>
+          );
+        }
       }
     }
+
+    console.log('render is about to return', playerSpans);
     return (
       <div>
         <h3>Waiting For Players</h3>
