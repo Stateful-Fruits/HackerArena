f5270e9e0f88bfabd78a1b185264a9001050629a
Simon K Zheng <simonkzheng@gmail.com>
2017-10-23 11:47:12 -0400

    History draft

--- hacker-arena/src/Components/History.js
+++ hacker-arena/src/Components/History.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import $ from 'jquery';
+import '../Styles/History.css';
 
 const History = (props) => {  
-  let games = props.profile.history;
-
+  let games = Object.values(props.profile.history);
+  
   return (
     <div>
+      {/* {games.map(item => {
+         return (
+          {item[0].winner === props.profile.username ? <p>"WIN"</p> : <p>"LOSS"</p>}
+          `<h3>${item[0].problem.title}</h3>`
+          `<p>${item[0].problem.description}</p>`
+          `<p>${item[0].problem.tags}</p>`
+          `<p>${item[0].timeTaken}</p>`
+          `<p>${item[0].timeStamp}</p>`)
+      })} */}
       
     </div>
 )};
