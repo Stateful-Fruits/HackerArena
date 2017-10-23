0076d32817048594919bddb96aa692060b09f7e9
David Zou <davidzsy20@Davids-MacBook-Pro.local>
2017-10-23 13:36:34 -0400

    video in paircoding

--- hacker-arena/src/Containers/Pair/NavigatorRoom.js
+++ hacker-arena/src/Containers/Pair/NavigatorRoom.js
@@ -5,7 +5,8 @@ import SpectatorEditor from '../../Components/Spectator/SpectatorEditor';
 import ProgressBar from '../../Components/GameRoom/ProgressBar';
 import TestSuite from '../../Components/TestSuite.js'; //From Simon
 import SpectatorEditors from '../../Components/Spectator/SpectatorEditors';
-
+import PairVideo from './PairVideo.js';
+import Helper from '../../Helpers/helpers.js'
 
 //this should better be a view instead of room;
 class NavigatorRoom extends React.Component {
@@ -18,9 +19,13 @@ class NavigatorRoom extends React.Component {
 
   render() {
     let { roomId, room, username, partnerName, partnerRole } = this.props
+  
+    let Videoroom = Helper._getTeamIndex(room, username)
+    
     return (
       <div>
         YOU ARE A NAVIGATOR - TALK!!
+        <PairVideo videoroom= {Videoroom}/>
         <ProgressBar room={ room }/>
         <div id="editorAndTestSuite">
           <SpectatorEditors 
