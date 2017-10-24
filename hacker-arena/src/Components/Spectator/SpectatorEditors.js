3c313069121f98b8cc79fff3f4bda9c1c7dbc446
ccolin84 <ccolin84@gmail.com>
2017-10-24 11:37:52 -0400

    spectator only sees editors for drivers

--- hacker-arena/src/Components/Spectator/SpectatorEditors.js
+++ hacker-arena/src/Components/Spectator/SpectatorEditors.js
@@ -8,7 +8,11 @@ class SpectatorEditors extends Component {
     let players = Object.keys(gameRoom.players)
     let partner = players.filter(playerName => playerName === partnerName) 
     let playersToShow = partner.length ? partner : players;
-    
+    if (gameRoom.isPairRoom) {
+      let teams = gameRoom.teams;
+      let navigators = teams.reduce((acc, team) => team.navigator ? [...acc, team.navigator] : [...acc], []);
+      playersToShow = playersToShow.filter((player) => !navigators.includes(player));
+    }
     return (
             <div>
               { playersToShow.map((playerName, i) => (
