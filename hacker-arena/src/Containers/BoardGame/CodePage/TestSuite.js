e3432571a3e3aec13b9e67b14bb71d4eed52b23f
kinyusui <kaixngchen@gmail.com>
2017-10-24 11:10:08 -0400

    powerups

--- hacker-arena/src/Containers/BoardGame/CodePage/TestSuite.js
+++ hacker-arena/src/Containers/BoardGame/CodePage/TestSuite.js
@@ -3,45 +3,26 @@ import fire from '../../../Firebase/firebase';
 import '../../../Styles/TestSuite.css';
 
 /* Check if user is challenger or creator*/
-const TestSuite = (props) => {
-  let username = fire.auth().currentUser.email.split('@')[0];
-  let {room} = props;
-  let userInfo = room.playerInfo[username];
-  let userPos = userInfo.position;
-  let problem = room.board[userPos[0]][userPos[1]][1];
-  if (problem) {
-    return(
-      <div id="testSuite">
-        <h3 className="problemTitle"> {problem.title} </h3>
-        <h4> PROBLEM DESCRIPTION  </h4>
-        <p id="description">{problem.description}</p>
-        <h4 className="testTitle"> TESTS </h4>
-        {(userInfo.testStatus && userInfo.testStatus.length > 1) ? 
-          (<div className="testHolder">{userInfo.testStatus.map((tests, i) => {
-            let passing = "PASSED!"
-            if(!tests.passed){
-              passing = "FAILED!"
-            }
-            return <div key={tests.inputs + i}>
-              {passing === "FAILED!" ? 
-              <span className="failure">{passing} </span>
-              : <span className="success">{passing} </span>
-              }
-              <span>{` Inputs: "${tests.inputs}" Expected: "${tests.expected}" Actual: "${tests.actual}"`}</span>
-            </div>
-          })}</div>) : null
-          }
-      </div>
-    )
-  } else {
-    return (
-      <div id="testSuite">
-      <h3 className="problemTitle"> No Problem Yet </h3>
+class TestSuite extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      testSuite: ''
+    }
+  }
+  componentDidMount () {
+    let username = fire.auth().currentUser.email.split('@')[0];
+    let {room} = this.props;
+    let userInfo = room.playerInfo[username];
+    let userPos = userInfo.position;
+    let problem = room.board[userPos[0]][userPos[1]][1];
+    let testSuite = <div id="testSuite">
+      <h3 className="problemTitle"> {problem.title} </h3>
       <h4> PROBLEM DESCRIPTION  </h4>
-      <p id="description"> No Problem Description Yet</p>
+      <p id="description">{problem.description}</p>
       <h4 className="testTitle"> TESTS </h4>
-      {(props.room.playerInfo[username].testStatus && props.room.players[username].testStatus.length > 1) ? 
-        (<div className="testHolder">{props.room.players[username].testStatus.map((tests, i) => {
+      {(userInfo.testStatus && userInfo.testStatus.length > 1) ? 
+        (<div className="testHolder">{userInfo.testStatus.map((tests, i) => {
           let passing = "PASSED!"
           if(!tests.passed){
             passing = "FAILED!"
@@ -56,12 +37,71 @@ const TestSuite = (props) => {
         })}</div>) : null
         }
     </div>
-
-
-
-    )
+    this.setState({
+      testSuite
+    });
+  }
+  render () {
+    let testSuite = this.state.testSuite;
+    return testSuite
   }
 }
 
-
 export default TestSuite;
+
+// const TestSuite = (props) => {
+//   let username = fire.auth().currentUser.email.split('@')[0];
+//   let {room} = props;
+//   let userInfo = room.playerInfo[username];
+//   let userPos = userInfo.position;
+//   let problem = room.board[userPos[0]][userPos[1]][1];
+//   if (problem) {
+//     return(
+//       <div id="testSuite">
+//         <h3 className="problemTitle"> {problem.title} </h3>
+//         <h4> PROBLEM DESCRIPTION  </h4>
+//         <p id="description">{problem.description}</p>
+//         <h4 className="testTitle"> TESTS </h4>
+//         {(userInfo.testStatus && userInfo.testStatus.length > 1) ? 
+//           (<div className="testHolder">{userInfo.testStatus.map((tests, i) => {
+//             let passing = "PASSED!"
+//             if(!tests.passed){
+//               passing = "FAILED!"
+//             }
+//             return <div key={tests.inputs + i}>
+//               {passing === "FAILED!" ? 
+//               <span className="failure">{passing} </span>
+//               : <span className="success">{passing} </span>
+//               }
+//               <span>{` Inputs: "${tests.inputs}" Expected: "${tests.expected}" Actual: "${tests.actual}"`}</span>
+//             </div>
+//           })}</div>) : null
+//           }
+//       </div>
+//     )
+//   } else {
+//     return (
+//       <div id="testSuite">
+//       <h3 className="problemTitle"> No Problem Yet </h3>
+//       <h4> PROBLEM DESCRIPTION  </h4>
+//       <p id="description"> No Problem Description Yet</p>
+//       <h4 className="testTitle"> TESTS </h4>
+//       {(props.room.playerInfo[username].testStatus && props.room.players[username].testStatus.length > 1) ? 
+//         (<div className="testHolder">{props.room.players[username].testStatus.map((tests, i) => {
+//           let passing = "PASSED!"
+//           if(!tests.passed){
+//             passing = "FAILED!"
+//           }
+//           return <div key={tests.inputs + i}>
+//             {passing === "FAILED!" ? 
+//             <span className="failure">{passing} </span>
+//             : <span className="success">{passing} </span>
+//             }
+//             <span>{` Inputs: "${tests.inputs}" Expected: "${tests.expected}" Actual: "${tests.actual}"`}</span>
+//           </div>
+//         })}</div>) : null
+//         }
+//     </div>
+//     )
+//   }
+// }
