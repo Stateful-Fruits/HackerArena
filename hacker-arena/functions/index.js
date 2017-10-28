e07c3db3b152150e8e9afc9a466613c6d7630e04
Paul Brown <paabrown@gmail.com>
2017-10-28 11:31:10 -0400

    Write function for giving me auth

--- hacker-arena/functions/index.js
+++ hacker-arena/functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +7,5 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+admin.auth().setCustomUserClaims('113023615147360761759', {admin: true}).then(() => console.log('set custom user claim'))
