4d872863e96a4ef3c963cbacb650eb655ac08890
ccolin84 <ccolin84@gmail.com>
2017-10-31 20:02:47 -0400

    update tests

--- hacker-arena/src/Tests/Login.test.js
+++ hacker-arena/src/Tests/Login.test.js
@@ -2,11 +2,13 @@ import nightmare from 'nightmare';
 
 describe('When on the login screen', () => {
   let page = nightmare().goto('http://localhost:3000')
-      .type('#Login > div > form > div.form-input > input', 'test123@gmail.com')
-      .type('#Login > div > form > div:nth-child(3) > input', 'test123')
-      .click('#Login > div > form > div:nth-child(5) > button').wait(1000);
+                        .type('#root > div > div > div > div.hero > div > div > form > div:nth-child(2) > input:nth-child(2)', 'test123@gmail.com')
+                        .type('#root > div > div > div > div.hero > div > div > form > div:nth-child(2) > input:nth-child(4)', 'test123')
+                        .click('#root > div > div > div > div.hero > div > div > form > div:nth-child(4) > button')
+                        .click('#root > div > nav > ul > li:nth-child(3)')
+                        .wait(1000).click('#root > div > nav > ul > li:nth-child(3)').screenshot('./src/Tests/test.png');
 
-  test('user can login and be redirected to homepage', async () => {
+  test('user can login and access the homepage', async () => {
     let gameRoomListShows = await page.exists('#GameRoomList').end();
     return expect(gameRoomListShows).toBe(true);
   }, 20000);
